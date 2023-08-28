660ab65a36dbdd33efccf48c99bec7a13b63910b
Gabriel Dupont Muenchen <gabriel.327m@gmail.com>
2023-08-28 17:37:32 -0300

    criando repo e projeto base

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+    return (
+      <header>
+        <h1>Bem vindo(a) ao portfólio de Gabriel Dupont Muenchen!</h1>
+        <nav>
+          {}
+        </nav>
+      </header>
+    );
+  };
+  
+  export default Header;
