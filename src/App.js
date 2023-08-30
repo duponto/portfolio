38592dcfac3401cb3962a76cc28330936c5cd4d7
Gabriel Dupont Muenchen <gabriel.327m@gmail.com>
2023-08-30 02:24:51 -0300

    utilizando fragment

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import Header from './components/Header';
 
 function App() {
   return (
-    <div>
-      <Header/>
-      <Body/>
-    </div>
+    <>
+      <Header />
+      <Body />
+    </>
   );
 }
 
--- src/components/FlexButton/index.js
+++ src/components/FlexButton/index.js
@@ -3,15 +3,15 @@ import { Button, Typography } from "@mui/material";
 
 const FlexButton = ({variant, href, customTheme, content}) => {
     return(
-            <Button 
-                variant={variant}
-                href={href}
-                color="inherit"
-                >
-                <Typography style={{fontFamily: customTheme}}>
-                    {content}
-                </Typography>
-            </Button>
+        <Button 
+            variant={variant}
+            href={href}
+            color="inherit"
+            >
+            <Typography style={{fontFamily: customTheme}}>
+                {content}
+            </Typography>
+        </Button>
     );
 }
 
